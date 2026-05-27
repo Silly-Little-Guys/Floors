9bf0106728de26da08565586226dc000d2e367b9
Rohan Bhattacharyya <dev.creator42@gmail.com>
2026-05-26 18:31:57 -0700

    basic unoptimized floor spawner

--- FloorSpawner.cs
+++ FloorSpawner.cs
@@ -1,15 +1,70 @@
 using Godot;
 using System;
+using System.Collections.Generic;
 
 public partial class FloorSpawner : Node2D
 {
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
+	[Export] public Player player;
+	[Export] public int level = 1;
+	private List<PackedScene> floors = new();
+	private const string floorsPath = "res://scenes/floors";
+
+	public void PopulateFloors(int level)
 	{
+		floors.Clear();
+
+		string levelPath = $"{floorsPath}/level{level}";
+		string[] fileNames = ResourceLoader.ListDirectory(levelPath);
+
+		if (fileNames.Length == 0)
+		{
+			GD.PushWarning($"No floor scenes found in: {levelPath}");
+			return;
+		}
+
+		foreach (string fileName in fileNames)
+		{
+			if (!fileName.EndsWith(".tscn", StringComparison.OrdinalIgnoreCase))
+			{
+				continue;
+			}
+
+			string scenePath = $"{levelPath}/{fileName}";
+			PackedScene floorScene = ResourceLoader.Load<PackedScene>(scenePath);
+
+			if (floorScene == null)
+			{
+				GD.PushWarning($"Could not load floor scene: {scenePath}");
+				continue;
+			}
+
+			floors.Add(floorScene);
+		}
+
+		ShuffleFloors();
 	}
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
+	private void ShuffleFloors()
 	{
+		for (int index = floors.Count - 1; index > 0; index--)
+		{
+			int swapIndex = GD.RandRange(0, index);
+			(floors[index], floors[swapIndex]) = (floors[swapIndex], floors[index]);
+		}
+	}
+
+	public override void _Ready()
+	{
+		PopulateFloors(level);
+	}
+	
+	public override void _PhysicsProcess(double delta)
+	{
+		
+	}
+
+	public void SpawnNextFloor()
+	{
+		
 	}
 }
