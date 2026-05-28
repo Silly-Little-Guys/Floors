ea74782447535ef63e84451052dfa75d4e69e1be
Eric Shu <45149431+ericshu6@users.noreply.github.com>
2026-05-28 16:13:10 -0700

    Remove debug print statement

--- scenes/hud/HUD.cs
+++ scenes/hud/HUD.cs
@@ -11,7 +11,6 @@ public partial class HUD : CanvasLayer
 	{
 		healthBar = GetNode<ProgressBar>("HealthBar");
 		healthBar.Value = player.GetHealth();
-		Console.WriteLine("Health: " + player.GetHealth());
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
