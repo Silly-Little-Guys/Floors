c317c94d0c6462a0b19975e1b4c4ea79defaa6ef
Yuan Xu <yuanx02122010@gmail.com>
2026-05-28 06:50:11 -0700

    Really,really,really,really basic enemy movement (No bueno pathfinding)

--- scenes/enemy/WingedEnemy.cs
+++ scenes/enemy/WingedEnemy.cs
@@ -1,10 +1,15 @@
 using Godot;
 using System;
 
-public partial class WingedEnemy : RigidBody2D, IEnemy
+public partial class WingedEnemy : CharacterBody2D, IEnemy
 {
-	public const float Speed = 300.0f;
-	public const float JumpVelocity = -400.0f;
+	public const float speed = 100.0f;
+	private Node2D player;
+
+    public override void _Ready()
+    {
+        player = (Node2D)GetNode("../Player");
+    }
 
     public void TakeDamage(int damage)
     {
@@ -13,33 +18,11 @@ public partial class WingedEnemy : RigidBody2D, IEnemy
 
     public override void _PhysicsProcess(double delta)
 	{
-		// Vector2 velocity = Velocity;
+		Vector2 velocity = Velocity;
+		Vector2 direction = (player.GlobalPosition - GlobalPosition).Normalized();
+		velocity = direction * speed;
 
-		// // Add the gravity.
-		// if (!IsOnFloor())
-		// {
-		// 	velocity += GetGravity() * (float)delta;
-		// }
-
-		// // Handle Jump.
-		// if (Input.IsActionJustPressed("ui_accept") && IsOnFloor())
-		// {
-		// 	velocity.Y = JumpVelocity;
-		// }
-
-		// // Get the input direction and handle the movement/deceleration.
-		// // As good practice, you should replace UI actions with custom gameplay actions.
-		// Vector2 direction = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
-		// if (direction != Vector2.Zero)
-		// {
-		// 	velocity.X = direction.X * Speed;
-		// }
-		// else
-		// {
-		// 	velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
-		// }
-
-		// Velocity = velocity;
-		// MoveAndSlide();
+		Velocity = velocity;
+		MoveAndSlide();
 	}
 }
