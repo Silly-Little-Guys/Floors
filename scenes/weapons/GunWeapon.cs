e2aa36cfe7119c916e56deed5aca327d5665bda5
RohanBhattacharyya <73246001+RohanBhattacharyya@users.noreply.github.com>
2026-05-29 12:36:16 -0700

    fix bullet direction

--- scenes/weapons/GunWeapon.cs
+++ scenes/weapons/GunWeapon.cs
@@ -11,10 +11,16 @@ public partial class GunWeapon : Weapon
 	[Export] public Node bulletSpawnPoint;
 	[Export] public AnimatedSprite2D animatedSprite2D;
 	[Export] public Node2D pivotPoint;
+	[Export] public RayCast2D shotDirection;
+	
+	private Vector2 pivotPointStartPosition;
+	private Vector2 shotDirectionStartPosition;
 
 	public override void _Ready()
 	{
 		fireTimer.WaitTime = 1 / fireRate;
+		pivotPointStartPosition = pivotPoint.Position;
+		shotDirectionStartPosition = shotDirection.Position;
 	}
 	public void OnShoot()
 	{
@@ -25,18 +31,22 @@ public partial class GunWeapon : Weapon
 		fireTimer.Start();
 		Bullet b = bulletScene.Instantiate<Bullet>();
 		b.damage = damage;
-		b.GlobalPosition = GlobalPosition;
-		b.Rotation = Rotation;
-		b.LinearVelocity = new Vector2(bulletSpeed, 0).Rotated(Rotation);
+		b.GlobalPosition = shotDirection.GlobalPosition;
+		b.Rotation = shotDirection.GlobalRotation;
+		b.LinearVelocity = new Vector2(bulletSpeed, 0).Rotated(shotDirection.GlobalRotation);
 		bulletSpawnPoint.AddChild(b);
 	}
 
 	public override void _PhysicsProcess(double delta)
 	{
+		bool shouldMirror = GetGlobalMousePosition().X < GlobalPosition.X;
+		animatedSprite2D.FlipV = shouldMirror;
+		pivotPoint.Position = new Vector2(pivotPointStartPosition.X, shouldMirror ? -pivotPointStartPosition.Y : pivotPointStartPosition.Y);
+		shotDirection.Position = new Vector2(shotDirectionStartPosition.X, shouldMirror ? -shotDirectionStartPosition.Y : shotDirectionStartPosition.Y);
+
 		Vector2 aimPivot = pivotPoint != null ? pivotPoint.GlobalPosition : GlobalPosition;
 		Vector2 aimDirection = GetGlobalMousePosition() - aimPivot;
 		GlobalRotation = aimDirection.Angle();
-		animatedSprite2D.FlipV = aimDirection.X < 0;
 		if (Input.IsActionPressed("shoot"))
 		{
 			OnShoot();
