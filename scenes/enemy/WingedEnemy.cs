bf6d643c06908d8fc722426658081d4aa2b1c06f
Yuan Xu <yuanx02122010@gmail.com>
2026-05-29 11:36:31 -0700

    Added WingedEnemy animation

--- scenes/enemy/WingedEnemy.cs
+++ scenes/enemy/WingedEnemy.cs
@@ -5,6 +5,8 @@ public partial class WingedEnemy : CharacterBody2D, IEnemy
 {
 	public const float speed = 100.0f;
 	public Player player;
+    string flightAnimation = "default";
+    [Export] public AnimatedSprite2D animatedSprite2D;
 
     public void TakeDamage(int damage)
     {
@@ -17,6 +19,7 @@ public partial class WingedEnemy : CharacterBody2D, IEnemy
 
     public override void _PhysicsProcess(double delta)
 	{
+        animatedSprite2D.Play(flightAnimation);
 		Vector2 velocity = Velocity;
 		Vector2 direction = (player.GlobalPosition - GlobalPosition).Normalized();
 		velocity = direction * speed;
