a64985675188bfb89641e387e176bc82e66bf943
Yuan Xu <yuanx02122010@gmail.com>
2026-06-04 09:55:29 -0700

    JumpyEnemy is ready to test

--- scenes/enemy/JumpyEnemy.cs
+++ scenes/enemy/JumpyEnemy.cs
@@ -4,39 +4,77 @@ using System;
 public partial class JumpyEnemy : RigidBody2D, IEnemy
 {
 	// Called when the node enters the scene tree for the first time.
+	public Player player;
 	[Export] public AudioStreamPlayer2D asp2d;
 	[Export] public EnemyHealthBar enemyHealthBar;
 	[Export] public CollisionShape2D collisionShape2D;
+	[Export] public AnimatedSprite2D animatedSprite2D;
+	[Export] public NavigationAgent2D nav;
+	[Export] public ShapeCast2D shape;
+	public const float speed = 30.0f;
+	public const float jumpForce = 200.0f;
+	public const float jumpRadius = 30.0f;
+	public const float gCompensation = 0.5f;
 	public int maxHealth;
+	string walk = "walk";
+	string state = "walking";
 	public override void _Ready()
-    {
-        maxHealth = GetHealth();
-    }
+	{
+		maxHealth = GetHealth();
+	}
 
 	public void TakeDamage(int damage)
-    {
-        SetMeta("Health", GetHealth() - damage);
-        if (GetHealth() <= 0)
-        {
-            asp2d.Play();
-            this.Visible = false;
-            // collisionShape2D.Disabled = true;
-            collisionShape2D.SetDeferred("disabled", true);
-        }
-        enemyHealthBar.SetProgress(Mathf.InverseLerp(0, maxHealth, GetHealth()));
-    }
+	{
+		SetMeta("Health", GetHealth() - damage);
+		if (GetHealth() <= 0)
+		{
+			asp2d.Play();
+			this.Visible = false;
+			collisionShape2D.SetDeferred("disabled", true);
+		}
+		enemyHealthBar.SetProgress(Mathf.InverseLerp(0, maxHealth, GetHealth()));
+	}
 
 	private int GetHealth()
-    {
-        return (int) GetMeta("Health");
-    }
+	{
+		return (int) GetMeta("Health");
+	}
 	public void OnAudioFinished()
 	{
 		QueueFree();
 	}
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
+	public override void _PhysicsProcess(double delta)
 	{
+		if (state.Equals("walking"))
+		{
+			nav.TargetPosition = player.GlobalPosition;
+			Vector2 nextPos = new Vector2(nav.GetNextPathPosition().X, GlobalPosition.Y);
+			animatedSprite2D.Play(walk);
+			ApplyCentralForce((nextPos - GlobalPosition).Normalized() * speed);
+
+			if (nextPos.X < GlobalPosition.X)
+			{
+				animatedSprite2D.FlipH = true;
+			} 
+			else if (nextPos.X > GlobalPosition.X)
+			{
+				animatedSprite2D.FlipH = false;
+			}
+
+			if ((nextPos - GlobalPosition).Length() < jumpRadius && shape.IsColliding())
+			{
+				state = "jumping";
+				ApplyCentralImpulse(((nextPos - GlobalPosition).Normalized() + Vector2.Up * gCompensation).Normalized() * jumpForce);
+			}
+		}
+		else if (state.Equals("jumping"))
+		{
+			if (shape.IsColliding())
+			{
+				state = "walking";
+			}
+		}
+		
 	}
 }
--- scenes/enemy/WingedEnemy.cs
+++ scenes/enemy/WingedEnemy.cs
@@ -13,30 +13,30 @@ public partial class WingedEnemy : CharacterBody2D, IEnemy
 	[Export] public CollisionShape2D collisionShape2D;
 	[Export] public NavigationAgent2D nav;
 
-    public override void _Ready()
-    {
-        maxHealth = GetHealth();
-    }
+	public override void _Ready()
+	{
+		maxHealth = GetHealth();
+	}
 
 	public void TakeDamage(int damage)
-    {
-        SetMeta("Health", GetHealth() - damage);
-        if (GetHealth() <= 0)
-        {
-            asp2d.Play();
-            this.Visible = false;
-            // collisionShape2D.Disabled = true;
-            collisionShape2D.SetDeferred("disabled", true);
-        }
-        enemyHealthBar.SetProgress(Mathf.InverseLerp(0, maxHealth, GetHealth()));
-    }
+	{
+		SetMeta("Health", GetHealth() - damage);
+		if (GetHealth() <= 0)
+		{
+			asp2d.Play();
+			this.Visible = false;
+			// collisionShape2D.Disabled = true;
+			collisionShape2D.SetDeferred("disabled", true);
+		}
+		enemyHealthBar.SetProgress(Mathf.InverseLerp(0, maxHealth, GetHealth()));
+	}
 
-    private int GetHealth()
-    {
-        return (int) GetMeta("Health");
-    }
+	private int GetHealth()
+	{
+		return (int) GetMeta("Health");
+	}
 
-    public void OnAudioFinished()
+	public void OnAudioFinished()
 	{
 		QueueFree();
 	}
