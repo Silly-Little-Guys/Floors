694417d09c3da94be854a9382a0f83e66ccc7c61
Rohan Bhattacharyya <73246001+RohanBhattacharyya@users.noreply.github.com>
2026-06-04 12:36:42 -0700

    pistol

--- scenes/weapons/GunWeapon.cs
+++ scenes/weapons/GunWeapon.cs
@@ -30,16 +30,18 @@ public partial class GunWeapon : Weapon
 		fireTimer.WaitTime = 1.0 / fireRate;
 		pivotPointStartPosition = pivotPoint.Position;
 		shotDirectionStartPosition = shotDirection.Position;
+		animatedSprite2D.SpriteFrames.SetAnimationLoop("firing", false);
+		animatedSprite2D.Play("idle");
 	}
-	public void OnShoot()
+	public bool OnShoot()
 	{
 		if (!fireTimer.IsStopped())
 		{
-			return;
+			return false;
 		}
 		if (ammoCount <= 0)
 		{
-			return;
+			return false;
 		}
 
 		ammoCount--;
@@ -63,6 +65,7 @@ public partial class GunWeapon : Weapon
 		}
 		player.Velocity += recoilVelocity;
 
+		return true;
 	}
 
 	public override void _PhysicsProcess(double delta)
@@ -75,11 +78,16 @@ public partial class GunWeapon : Weapon
 		Vector2 aimPivot = pivotPoint != null ? pivotPoint.GlobalPosition : GlobalPosition;
 		Vector2 aimDirection = GetGlobalMousePosition() - aimPivot;
 		GlobalRotation = aimDirection.Angle();
+		bool firedThisFrame = false;
 		if (Input.IsActionPressed("shoot"))
 		{
-			OnShoot();
+			firedThisFrame = OnShoot();
+		}
+		if (firedThisFrame)
+		{
+			animatedSprite2D.Stop();
 			animatedSprite2D.Play("firing");
-		} else
+		} else if (animatedSprite2D.Animation != "firing" || !animatedSprite2D.IsPlaying())
 		{
 			animatedSprite2D.Play("idle");   
 		}
