bd25ffe51dc2c94a1179d7412057863b48548588
Eric Shu <45149431+ericshu6@users.noreply.github.com>
2026-06-05 15:03:49 -0700

    Slightly improved death screen

---
+++ scenes/death_screen/DeathScreen.cs
@@ -0,0 +1,30 @@
+using Godot;
+using System;
+
+public partial class DeathScreen : Control
+{
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+
+	public void OnStartButtonPressed()
+	{
+		GetTree().ChangeSceneToFile("res://main.tscn");
+	}
+	
+	public void OnMainMenuButtonPressed()
+	{
+		GetTree().ChangeSceneToFile("res://scenes/title_screen/TitleScreen.tscn");
+	}
+
+	public void OnQuitButtonPressed()
+	{
+		GetTree().Quit();
+	}
+}
