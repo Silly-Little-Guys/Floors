5b217e731a8b08b9bcf4f49c27ae974f9c91182b
Eric Shu <45149431+ericshu6@users.noreply.github.com>
2026-06-06 00:13:38 -0700

    Weapons in Lootbox stay quiet and animate

--- scenes/lootbox/Lootbox.cs
+++ scenes/lootbox/Lootbox.cs
@@ -33,9 +33,16 @@ public partial class Lootbox : Node2D, IInteractable
 		opened = true;
 		chestSprite.Frame = 1;
 		Node itemNode = item.ItemScene.Instantiate();
-		itemSprite.AddChild(itemNode);
-		itemSprite.GetChild(0).ProcessMode = ProcessModeEnum.Disabled;
 
+		foreach (Node node in itemNode.GetChildren())
+		{
+			if (node is AudioStreamPlayer2D asp2d)
+			{
+				asp2d.ProcessMode = ProcessModeEnum.Disabled;
+			}
+		} // Disable all audio streams in mini item scene (because it's just previewing the item)
+
+		itemSprite.AddChild(itemNode);
 		openSoundPlayer.Play();
 
 		return null; // So player does nothing else, only opens chest
